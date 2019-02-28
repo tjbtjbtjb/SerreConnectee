220f29d6334ac4ebfab3c0ba432ac8059311a6ea
Tristan Beau <tristan.beau@univ-paris-diderot.fr>
2019-02-28 10:11:30 +0100

    Protect the getValue() function. Need to use the getLastValue which return the last recorded value is not to old (i.e. evaluted within sm_maxTime)

--- Arduino/GreenHouseManager/Sensor.h
+++ Arduino/GreenHouseManager/Sensor.h
@@ -16,9 +16,25 @@
 
 class Sensor : public Device {
   public:
-    Sensor(String s) : Device(s) {} ;
-    virtual float getValue() = 0 ;
+    Sensor(String s) : Device(s),m_lastTime(0),m_lastVal(-1.) {} ;
+    float getLastValue() { 
+      unsigned long now = millis();
+      if ( now - m_lastTime > sm_maxTime ) {
+        m_lastVal = getValue();
+        m_lastTime = now; 
+      }
+      return m_lastVal;
+    }
     virtual ~Sensor() {} ;
+    
+  protected : 
+    virtual float getValue() = 0 ;  // protected in order to force the use of getLastValue()
+     
+  private:
+    float m_lastVal;
+    unsigned long m_lastTime;
+    const static unsigned long sm_maxTime ;
 };
+
 
 #endif
